345b6c8469259312d839074410fc6f9b18e93249
Sanskar Amgain <sanskaramgain@gmail.com>
2022-04-17 06:36:05 +0545

    Mandelbrot position changed

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var mandelbrot_element = document.querySelector("#mandlebrot");
 mandelbrot_element.width = window.innerWidth;
 mandelbrot_element.height = window.innerHeight;
-var rect_min = [-2.5, -2.0];
-var rect_max = [2, 1.5];
+var rect_min = [-0.58, -0.5435277544383506];
+var rect_max = [-0.5155981075602742, -0.5005446357189197];
 var vertexSource = "\n\tattribute vec2 vertex;\n\n\tvoid main(){\n\t\tgl_Position = vec4(vertex, 0.0, 1.0);\n\t}\n";
-var fragmentSource = "\n\tprecision highp float;\n\tprecision highp int;\n\tuniform vec2 rectMin;\n\tuniform vec2 rectMax;\n\tuniform float width;\n\tuniform float height;\n\tuniform vec2 resolution;\n\n\tstruct Complex{\n\t\tfloat real, imag;\n\t}; \n\t\n\t\n\tfloat magnitude(vec2 v){\n\t\treturn pow(v.x * v.x + v.y * v.y, 0.5);\n\t}\n\t\n\t#define MAX_ITERATIONS 250\n\t#define cproduct(a, b) vec2(a.x*b.x-a.y*b.y, a.x*b.y+a.y*b.x)\n\n\tfloat Radius = 5.0;\n\tvec3 ColorWeight = vec3(3.0, 4.0, 5.0);\n\n\tint Diverge(inout vec2 c, float radius) {\n\t\tvec2 z = vec2(0, 0);\n\t\tint iter = 0;\n\t\tfor(int i = 0; i < MAX_ITERATIONS; i++) {\n\t\t\tz = cproduct(z, z) + c;\n\t\t\titer += 1;\n\n\t\t\tif(length(z) >= radius) {\n\t\t\t\tbreak;\n\t\t\t} \n\t\t}\n\t\tc = z;\n\t\treturn iter;\n\t}\n\n\t#define brightness 2.0\n\n\tvoid main() {\n\t\tvec2 st = vec2(gl_FragCoord.x / width, gl_FragCoord.y / height);\n\t\tfloat aspect_ratio = width / height;\n\t\tvec2 z = rectMin + st * (rectMax - rectMin);\n\t\tint iterations = Diverge(z, Radius);\n\t\tfloat luminance = ((float(iterations) - log2(length(z) / float(Radius))) / float(MAX_ITERATIONS));\n\t\tvec3 color = ColorWeight * luminance;\n\t\tgl_FragColor = vec4(color, 1);\n  \t}\n";
+var fragmentSource = "\n\tprecision highp float;\n\tprecision highp int;\n\tuniform vec2 rectMin;\n\tuniform vec2 rectMax;\n\tuniform float width;\n\tuniform float height;\n\tuniform vec2 resolution;\n\n\tstruct Complex{\n\t\tfloat real, imag;\n\t}; \n\t\n\t\n\tfloat magnitude(vec2 v){\n\t\treturn pow(v.x * v.x + v.y * v.y, 0.5);\n\t}\n\t\n\t#define MAX_ITERATIONS 800\n\t#define cproduct(a, b) vec2(a.x*b.x-a.y*b.y, a.x*b.y+a.y*b.x)\n\n\tfloat Radius = 7.5;\n\tvec3 ColorWeight = vec3(3.0, 4.0, 5.0);\n\n\tint Diverge(inout vec2 c, float radius) {\n\t\tvec2 z = vec2(0, 0);\n\t\tint iter = 0;\n\t\tfor(int i = 0; i < MAX_ITERATIONS; i++) {\n\t\t\tz = cproduct(z, z) + c;\n\t\t\titer += 1;\n\n\t\t\tif(length(z) >= radius) {\n\t\t\t\tbreak;\n\t\t\t} \n\t\t}\n\t\tc = z;\n\t\treturn iter;\n\t}\n\n\t#define brightness 2.0\n\n\tvoid main() {\n\t\tvec2 st = vec2(gl_FragCoord.x / width, gl_FragCoord.y / height);\n\t\tfloat aspect_ratio = width / height;\n\t\tvec2 z = rectMin + st * (rectMax - rectMin);\n\t\tint iterations = Diverge(z, Radius);\n\t\tfloat luminance = ((float(iterations) - log2(length(z) / float(Radius))) / float(MAX_ITERATIONS));\n\t\tvec3 color = ColorWeight * luminance;\n\t\tgl_FragColor = vec4(color, 1);\n  \t}\n";
 function load_shader(gl, type, source) {
     var shader = gl.createShader(type);
     gl.shaderSource(shader, source);
@@ -97,4 +97,20 @@ document.addEventListener('scroll', function (e) {
     handleScrollPosition(window.scrollY);
     main();
     lastKnownScrollPosition = window.scrollY;
+});
+document.addEventListener('dblclick', function (event) {
+    var canvasWidth = mandelbrot_element.width;
+    var canvasHeight = mandelbrot_element.height;
+    var zoomsize = 2;
+    var selectedWidth = canvasWidth / zoomsize;
+    var selectedHeight = canvasHeight / zoomsize;
+    var initialX = (event.clientX - (selectedWidth / 2)) / canvasWidth;
+    var finalX = (event.clientX + (selectedWidth / 2)) / canvasWidth;
+    var initialY = (event.clientY - (selectedHeight / 2)) / canvasHeight;
+    var finalY = (event.clientY + (selectedHeight / 2)) / canvasHeight;
+    rect_min[0] = ((rect_max[0] - rect_min[0]) * initialX) + rect_min[0],
+        rect_max[0] = ((rect_max[0] - rect_min[0]) * finalX) + rect_min[0],
+        rect_min[1] = ((rect_max[1] - rect_min[0]) * initialY) + rect_min[0];
+    rect_max[1] = ((rect_max[1] - rect_min[0]) * finalY) + rect_min[0];
+    main();
 });
