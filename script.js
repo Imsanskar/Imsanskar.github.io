9e288331229da514a3c041cd82aa7bc1a817fd6e
Sanskar Amgain <sanskaramgain@gmail.com>
2022-09-24 09:15:13 +0545

    Panning, zoom in out added

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var fragmentSource = "\n\tprecision highp float;\n\tprecision highp int;\n\tuniform vec2 rectMin;\n\tuniform vec2 rectMax;\n\tuniform float width;\n\tuniform float height;\n\tuniform vec2 resolution;\n\n\tstruct Complex{\n\t\tfloat real, imag;\n\t}; \n\t\n\t\n\tfloat magnitude(vec2 v){\n\t\treturn pow(v.x * v.x + v.y * v.y, 0.5);\n\t}\n\t\n\t#define MAX_ITERATIONS 2000\n\t#define cproduct(a, b) vec2(a.x*b.x-a.y*b.y, a.x*b.y+a.y*b.x)\n\n\tfloat Radius = 4.0;\n\tvec3 ColorWeight = vec3(4.0, 4.0, 6.9);\n\n\tint Diverge(inout vec2 c, float radius) {\n\t\tvec2 z = vec2(0, 0);\n\t\tint iter = 0;\n\t\tfor(int i = 0; i < MAX_ITERATIONS; i++) {\n\t\t\tz = cproduct(z, z) + c;\n\t\t\titer += 1;\n\n\t\t\tif(length(z) >= radius) {\n\t\t\t\tbreak;\n\t\t\t} \n\t\t}\n\t\tc = z;\n\t\treturn iter;\n\t}\n\n\t#define brightness 6.9\n\n\tvec3 lerp(vec3 v0, vec3 v1, vec3 t) {\n\t\treturn v0 + t * (v1 - v0);\n\t}\n\n\t\n\tfloat lerp(float v0, float v1, float t) {\n\t\treturn v0 + t * (v1 - v0);\n\t}\n\n\tvec3 hsv2rgb(vec3 c) {\n\t\tvec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);\n\t\tvec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);\n\t\treturn c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);\n\t}\n\n\tvec3 SmoothColoring(vec2 c, float radius) {\t\n\t\tconst float Saturation = 1.0;\n\t\tconst float Value = 0.8;\n\t\tconst float MinHue = 0.1;\n\t\tconst float MaxHue = 0.8;\n\n\t\tvec2 z = vec2(0, 0);\n\t\tint iterations = 0;\n\t\tfor(int i = 0; i < MAX_ITERATIONS; i++) {\n\t\t\tz = cproduct(z, z) + c;\n\t\t\titerations += 1;\n\n\t\t\tif(length(z) >= radius) {\n\t\t\t\tbreak;\n\t\t\t} \n\t\t}\n\n\t\tfloat value = 0.0;\n\t\tif (iterations < MAX_ITERATIONS) {\n\t\t\tfloat log_zn = log(z.x * z.x + z.y * z.y) / 2.0;\n\t\t\tfloat nu = log(log_zn / log(2.0)) / log(2.0);\n\t\t\t// Rearranging the potential function.\n\t\t\t// Dividing log_zn by log(2) instead of log(N = 1<<8)\n\t\t\t// because we want the entire palette to range from the\n\t\t\t// center to radius 2, NOT our bailout radius.\n\t\t\titerations = iterations + 1 - int(nu);\n\t\t\tvalue = Value;\n\t\t}\n\t\t\n\t\tfloat hue1 = float(iterations) / float(MAX_ITERATIONS);\n\t\tfloat hue2 = float(iterations + 1) / float(MAX_ITERATIONS);\n\t\tfloat hue = lerp(hue1, hue2, 1.0);\n\t\thue = MinHue + hue * (MaxHue - MinHue);\n\n\t\tvec3 color = hsv2rgb(vec3(hue, Saturation, value)); \n\t\treturn color;\n\t}\n\n\tvec3 WaveColoring(vec2 c, float radius) {\n\t\tvec2 z = vec2(0, 0);\n\t\tint iterations = 0;\n\t\tfor(int i = 0; i < MAX_ITERATIONS; i++) {\n\t\t\tz = cproduct(z, z) + c;\n\t\t\titerations += 1;\n\n\t\t\tif(length(z) >= radius) {\n\t\t\t\tbreak;\n\t\t\t} \n\t\t}\n\n\t\tvec3 color;\n\t\tconst float Amount = 0.7;\n\t\tcolor.z = 0.5 * sin(Amount * float(iterations) + 4.188) + 0.5;\n\t\tcolor.x = 0.5 * sin(Amount * float(iterations)) + 0.5;\n\t\tcolor.y = 0.5 * sin(Amount * float(iterations) + 2.094) + 0.5;\n\t\t\n\t\treturn color;\n\t}\n\n\tvoid main() {\n\t\tvec2 st = vec2(gl_FragCoord.x / width, gl_FragCoord.y / height);\n\t\tfloat aspect_ratio = width / height;\n\t\tvec2 z = rectMin + st * (rectMax - rectMin) * vec2(aspect_ratio, 1);\n\t\t// int iterations = Diverge(z, Radius);\n\t\t// float luminance = ((float(iterations) - log2(length(z) / float(Radius))) / float(MAX_ITERATIONS));\n\t\t// vec3 color = ColorWeight * luminance;\n\t\tgl_FragColor = vec4(WaveColoring(z, Radius), 1);\n  \t}\n";
 var mandelbrot_element = document.querySelector("#mandlebrot");
 mandelbrot_element.width = window.innerWidth;
 mandelbrot_element.height = window.innerHeight;
-// let rect_min = [-0.5469222835916808, -0.5460876992863223]
-// let rect_max = [-0.5428804992200343, -0.541211969923438]
-// let rect_min = [-0.5649658399209594, -0.5632633125141023]
-// let rect_max = [-0.5597451079968162, -0.5530003578299604 ]
-// -0.5469222835916808, -0.5460876992863223
-// [-0.5155981075602742, -0.5005446357189197]
-var rect_max = [-0.5432966514872561, -0.542811840814511];
-var rect_min = [-0.5455350253099542, -0.544973368507769];
+var rect_max = [-0.2794752118323775, -0.5297359079192027];
+var rect_min = [-0.6425349277723049, -0.6626432440196919];
 var vertexSource = "\n\tattribute vec2 vertex;\n\n\tvoid main(){\n\t\tgl_Position = vec4(vertex, 0.0, 1.0);\n\t}\n";
-var fragmentSource = "\n\tprecision highp float;\n\tprecision highp int;\n\tuniform vec2 rectMin;\n\tuniform vec2 rectMax;\n\tuniform float width;\n\tuniform float height;\n\tuniform vec2 resolution;\n\n\tstruct Complex{\n\t\tfloat real, imag;\n\t}; \n\t\n\t\n\tfloat magnitude(vec2 v){\n\t\treturn pow(v.x * v.x + v.y * v.y, 0.5);\n\t}\n\t\n\t#define MAX_ITERATIONS 3000\n\t#define cproduct(a, b) vec2(a.x*b.x-a.y*b.y, a.x*b.y+a.y*b.x)\n\n\tfloat Radius = 5.0;\n\tvec3 ColorWeight = vec3(4.0, 4.0, 6.9);\n\n\tint Diverge(inout vec2 c, float radius) {\n\t\tvec2 z = vec2(0, 0);\n\t\tint iter = 0;\n\t\tfor(int i = 0; i < MAX_ITERATIONS; i++) {\n\t\t\tz = cproduct(z, z) + c;\n\t\t\titer += 1;\n\n\t\t\tif(length(z) >= radius) {\n\t\t\t\tbreak;\n\t\t\t} \n\t\t}\n\t\tc = z;\n\t\treturn iter;\n\t}\n\n\t#define brightness 6.9\n\n\tvoid main() {\n\t\tvec2 st = vec2(gl_FragCoord.x / width, gl_FragCoord.y / height);\n\t\tfloat aspect_ratio = width / height;\n\t\tvec2 z = rectMin + st * (rectMax - rectMin) * vec2(aspect_ratio, 1);\n\t\tint iterations = Diverge(z, Radius);\n\t\tfloat luminance = ((float(iterations) - log2(length(z) / float(Radius))) / float(MAX_ITERATIONS));\n\t\tvec3 color = ColorWeight * luminance;\n\t\tgl_FragColor = vec4(color, 1);\n  \t}\n";
 function load_shader(gl, type, source) {
     var shader = gl.createShader(type);
     gl.shaderSource(shader, source);
@@ -49,22 +79,28 @@ function init_buffer(gl) {
         position: position_buffer
     };
 }
+var delay = function (ms) { return new Promise(function (resolve) { return setTimeout(resolve, ms); }); };
 function render(gl, program_info, buffers) {
-    gl.clearColor(0.0, 0.0, 0.0, 1.0); // Clear to black, fully opaque
-    gl.clearDepth(1.0); // Clear everything
-    gl.enable(gl.DEPTH_TEST); // Enable depth testing
-    gl.depthFunc(gl.LEQUAL); // Near things obscure far things
-    // Clear the canvas before we start drawing on it.
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
-    gl.vertexAttribPointer(program_info.attrib_location.vertex_position, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(program_info.attrib_location.vertex_position);
-    gl.useProgram(program_info.program);
-    gl.uniform2f(program_info.uniform_locations.rect_min, rect_min[0], rect_min[1]);
-    gl.uniform2f(program_info.uniform_locations.rect_max, rect_max[0], rect_max[1]);
-    gl.uniform1f(program_info.uniform_locations.width, window.innerWidth);
-    gl.uniform1f(program_info.uniform_locations.height, window.innerHeight);
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+    return __awaiter(this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            gl.clearColor(0.0, 0.0, 0.0, 1.0); // Clear to black, fully opaque
+            gl.clearDepth(1.0); // Clear everything
+            gl.enable(gl.DEPTH_TEST); // Enable depth testing
+            gl.depthFunc(gl.LEQUAL); // Near things obscure far things
+            // Clear the canvas before we start drawing on it.
+            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+            gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+            gl.vertexAttribPointer(program_info.attrib_location.vertex_position, 2, gl.FLOAT, false, 0, 0);
+            gl.enableVertexAttribArray(program_info.attrib_location.vertex_position);
+            gl.useProgram(program_info.program);
+            gl.uniform2f(program_info.uniform_locations.rect_min, rect_min[0], rect_min[1]);
+            gl.uniform2f(program_info.uniform_locations.rect_max, rect_max[0], rect_max[1]);
+            gl.uniform1f(program_info.uniform_locations.width, window.innerWidth);
+            gl.uniform1f(program_info.uniform_locations.height, window.innerHeight);
+            gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+            return [2 /*return*/];
+        });
+    });
 }
 function main() {
     var gl = mandelbrot_element.getContext("webgl");
@@ -89,6 +125,15 @@ function main() {
     render(gl, program_info, position_buffer);
 }
 requestAnimationFrame(main);
+function MapRange(from_x1, from_x2, to_x1, to_x2, x) {
+    return (to_x2 - to_x1) / (from_x2 - from_x1) * (x - from_x1) + to_x1;
+}
+var mouse_x = 0;
+var mouse_y = 0;
+document.addEventListener("mousemove", function (event) {
+    mouse_x = event.clientX;
+    mouse_y = event.clientY;
+});
 document.addEventListener('dblclick', function (event) {
     var canvasWidth = mandelbrot_element.width;
     var canvasHeight = mandelbrot_element.height;
@@ -103,5 +148,64 @@ document.addEventListener('dblclick', function (event) {
         rect_max[0] = ((rect_max[0] - rect_min[0]) * finalX) + rect_min[0],
         rect_min[1] = ((rect_max[1] - rect_min[0]) * initialY) + rect_min[0];
     rect_max[1] = ((rect_max[1] - rect_min[0]) * finalY) + rect_min[0];
+    main();
+});
+document.addEventListener('scroll', function (event) {
+    var canvasWidth = mandelbrot_element.width;
+    var canvasHeight = mandelbrot_element.height;
+    var cx = MapRange(0, canvasWidth, rect_min[0], rect_max[0], mouse_x);
+    var cy = MapRange(0, canvasHeight, rect_min[1], rect_max[1], canvasHeight - mouse_y);
+    rect_min[0] -= cx;
+    rect_max[0] -= cx;
+    rect_min[1] -= cy;
+    rect_max[1] -= cy;
+    var factor = -2;
+    rect_min[0] *= factor;
+    rect_max[0] *= factor;
+    rect_min[1] *= factor;
+    rect_max[1] *= factor;
+    rect_min[0] += cx;
+    rect_max[0] += cx;
+    rect_min[1] += cy;
+    rect_max[1] += cy;
+    main();
+});
+document.addEventListener('keypress', function (event) {
+    var factor = 0.01;
+    if (event.key == 'w') {
+        rect_max[1] += factor;
+        rect_min[1] += factor;
+    }
+    else if (event.key == 's') {
+        rect_max[1] -= factor;
+        rect_min[1] -= factor;
+    }
+    else if (event.key == 'a') {
+        rect_max[0] -= factor;
+        rect_min[0] -= factor;
+    }
+    else if (event.key == 'd') {
+        rect_max[0] += factor;
+        rect_min[0] += factor;
+    }
+    else if (event.key == 'o') {
+        var canvasWidth = mandelbrot_element.width;
+        var canvasHeight = mandelbrot_element.height;
+        var cx = MapRange(0, canvasWidth, rect_min[0], rect_max[0], mouse_x);
+        var cy = MapRange(0, canvasHeight, rect_min[1], rect_max[1], canvasHeight - mouse_y);
+        rect_min[0] -= cx;
+        rect_max[0] -= cx;
+        rect_min[1] -= cy;
+        rect_max[1] -= cy;
+        var factor_1 = 1.1;
+        rect_min[0] *= factor_1;
+        rect_max[0] *= factor_1;
+        rect_min[1] *= factor_1;
+        rect_max[1] *= factor_1;
+        rect_min[0] += cx;
+        rect_max[0] += cx;
+        rect_min[1] += cy;
+        rect_max[1] += cy;
+    }
     main();
 });
--- script.ts
+++ script.ts
@@ -1,27 +1,3 @@
-let mandelbrot_element:HTMLCanvasElement = document.querySelector("#mandlebrot");
-mandelbrot_element.width = window.innerWidth
-mandelbrot_element.height = window.innerHeight
-
-// let rect_min = [-0.5469222835916808, -0.5460876992863223]
-// let rect_max = [-0.5428804992200343, -0.541211969923438]
-
-// let rect_min = [-0.5649658399209594, -0.5632633125141023]
-// let rect_max = [-0.5597451079968162, -0.5530003578299604 ]
-
-// -0.5469222835916808, -0.5460876992863223
-// [-0.5155981075602742, -0.5005446357189197]
-
-let rect_max = [-0.5432966514872561, -0.542811840814511]
-let rect_min = [-0.5455350253099542, -0.544973368507769]
-
-const vertexSource = `
-	attribute vec2 vertex;
-
-	void main(){
-		gl_Position = vec4(vertex, 0.0, 1.0);
-	}
-`
-
 const fragmentSource = `
 	precision highp float;
 	precision highp int;
@@ -40,10 +16,10 @@ const fragmentSource = `
 		return pow(v.x * v.x + v.y * v.y, 0.5);
 	}
 	
-	#define MAX_ITERATIONS 3000
+	#define MAX_ITERATIONS 2000
 	#define cproduct(a, b) vec2(a.x*b.x-a.y*b.y, a.x*b.y+a.y*b.x)
 
-	float Radius = 5.0;
+	float Radius = 4.0;
 	vec3 ColorWeight = vec3(4.0, 4.0, 6.9);
 
 	int Diverge(inout vec2 c, float radius) {
@@ -63,15 +39,107 @@ const fragmentSource = `
 
 	#define brightness 6.9
 
+	vec3 lerp(vec3 v0, vec3 v1, vec3 t) {
+		return v0 + t * (v1 - v0);
+	}
+
+	
+	float lerp(float v0, float v1, float t) {
+		return v0 + t * (v1 - v0);
+	}
+
+	vec3 hsv2rgb(vec3 c) {
+		vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+		vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+		return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+	}
+
+	vec3 SmoothColoring(vec2 c, float radius) {	
+		const float Saturation = 1.0;
+		const float Value = 0.8;
+		const float MinHue = 0.1;
+		const float MaxHue = 0.8;
+
+		vec2 z = vec2(0, 0);
+		int iterations = 0;
+		for(int i = 0; i < MAX_ITERATIONS; i++) {
+			z = cproduct(z, z) + c;
+			iterations += 1;
+
+			if(length(z) >= radius) {
+				break;
+			} 
+		}
+
+		float value = 0.0;
+		if (iterations < MAX_ITERATIONS) {
+			float log_zn = log(z.x * z.x + z.y * z.y) / 2.0;
+			float nu = log(log_zn / log(2.0)) / log(2.0);
+			// Rearranging the potential function.
+			// Dividing log_zn by log(2) instead of log(N = 1<<8)
+			// because we want the entire palette to range from the
+			// center to radius 2, NOT our bailout radius.
+			iterations = iterations + 1 - int(nu);
+			value = Value;
+		}
+		
+		float hue1 = float(iterations) / float(MAX_ITERATIONS);
+		float hue2 = float(iterations + 1) / float(MAX_ITERATIONS);
+		float hue = lerp(hue1, hue2, 1.0);
+		hue = MinHue + hue * (MaxHue - MinHue);
+
+		vec3 color = hsv2rgb(vec3(hue, Saturation, value)); 
+		return color;
+	}
+
+	vec3 WaveColoring(vec2 c, float radius) {
+		vec2 z = vec2(0, 0);
+		int iterations = 0;
+		for(int i = 0; i < MAX_ITERATIONS; i++) {
+			z = cproduct(z, z) + c;
+			iterations += 1;
+
+			if(length(z) >= radius) {
+				break;
+			} 
+		}
+
+		vec3 color;
+		const float Amount = 0.7;
+		color.z = 0.5 * sin(Amount * float(iterations) + 4.188) + 0.5;
+		color.x = 0.5 * sin(Amount * float(iterations)) + 0.5;
+		color.y = 0.5 * sin(Amount * float(iterations) + 2.094) + 0.5;
+		
+		return color;
+	}
+
 	void main() {
 		vec2 st = vec2(gl_FragCoord.x / width, gl_FragCoord.y / height);
 		float aspect_ratio = width / height;
 		vec2 z = rectMin + st * (rectMax - rectMin) * vec2(aspect_ratio, 1);
-		int iterations = Diverge(z, Radius);
-		float luminance = ((float(iterations) - log2(length(z) / float(Radius))) / float(MAX_ITERATIONS));
-		vec3 color = ColorWeight * luminance;
-		gl_FragColor = vec4(color, 1);
+		// int iterations = Diverge(z, Radius);
+		// float luminance = ((float(iterations) - log2(length(z) / float(Radius))) / float(MAX_ITERATIONS));
+		// vec3 color = ColorWeight * luminance;
+		gl_FragColor = vec4(WaveColoring(z, Radius), 1);
   	}
+`
+
+
+
+
+let mandelbrot_element:HTMLCanvasElement = document.querySelector("#mandlebrot");
+mandelbrot_element.width = window.innerWidth
+mandelbrot_element.height = window.innerHeight
+
+let rect_max = [-0.2794752118323775, -0.5297359079192027]
+let rect_min = [-0.6425349277723049, -0.6626432440196919]
+
+const vertexSource = `
+	attribute vec2 vertex;
+
+	void main(){
+		gl_Position = vec4(vertex, 0.0, 1.0);
+	}
 `
 
 function load_shader(gl:WebGLRenderingContext, type:number, source:string): WebGLShader | null {
@@ -130,7 +198,9 @@ function init_buffer(gl:WebGLRenderingContext) {
 
 }
 
-function render(gl:WebGLRenderingContext, program_info, buffers) {
+let delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+async function render(gl:WebGLRenderingContext, program_info, buffers) {
 	gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
 	gl.clearDepth(1.0);                 // Clear everything
 	gl.enable(gl.DEPTH_TEST);           // Enable depth testing
@@ -165,8 +235,6 @@ function render(gl:WebGLRenderingContext, program_info, buffers) {
 	gl.uniform1f(
 		program_info.uniform_locations.height, window.innerHeight
 	)
-
-
 	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
 } 
 
@@ -197,6 +265,16 @@ function main() {
 requestAnimationFrame(main)
 
 
+function MapRange(from_x1: number, from_x2: number, to_x1: number, to_x2: number, x: number): number {
+    return (to_x2 - to_x1) / (from_x2 - from_x1) * (x - from_x1) + to_x1;
+}
+
+var mouse_x = 0
+var mouse_y = 0
+document.addEventListener("mousemove", event => {
+	mouse_x = event.clientX
+	mouse_y = event.clientY
+})
 
 document.addEventListener('dblclick', event => {
 	let canvasWidth = mandelbrot_element.width
@@ -216,6 +294,74 @@ document.addEventListener('dblclick', event => {
 	
 	rect_min[1] = ((rect_max[1] - rect_min[0]) * initialY) + rect_min[0]
 	rect_max[1] = ((rect_max[1] - rect_min[0]) * finalY) + rect_min[0]
+
+	main()
+})
+
+document.addEventListener('scroll', event => {
+	let canvasWidth = mandelbrot_element.width
+	let canvasHeight = mandelbrot_element.height
+
+	let cx: number = MapRange(0, canvasWidth, rect_min[0], rect_max[0], mouse_x);
+	let cy: number = MapRange(0, canvasHeight, rect_min[1], rect_max[1], canvasHeight - mouse_y);
+	rect_min[0] -= cx;
+	rect_max[0] -= cx;
+	rect_min[1] -= cy;
+	rect_max[1] -= cy;
+
+	let factor = -2;
+
+	rect_min[0] *= factor;
+	rect_max[0] *= factor;
+	rect_min[1] *= factor;
+	rect_max[1] *= factor;
+	
+	rect_min[0] += cx;
+	rect_max[0] += cx;
+	rect_min[1] += cy;
+	rect_max[1] += cy;
+
+	main()
+})
+
+document.addEventListener('keypress', event => {
+	let factor = 0.01
+	if (event.key == 'w') {
+		rect_max[1] += factor
+		rect_min[1] += factor 
+	} else if (event.key == 's') {
+		rect_max[1] -= factor
+		rect_min[1] -= factor 
+	} else if (event.key == 'a') {
+		rect_max[0] -= factor
+		rect_min[0] -= factor 
+	} else if (event.key == 'd') {
+		rect_max[0] += factor
+		rect_min[0] += factor 
+	} else if (event.key == 'o') {
+		let canvasWidth = mandelbrot_element.width
+		let canvasHeight = mandelbrot_element.height
+
+		let cx: number = MapRange(0, canvasWidth, rect_min[0], rect_max[0], mouse_x);
+		let cy: number = MapRange(0, canvasHeight, rect_min[1], rect_max[1], canvasHeight - mouse_y);
+		rect_min[0] -= cx;
+		rect_max[0] -= cx;
+		rect_min[1] -= cy;
+		rect_max[1] -= cy;
+
+		let factor = 1.1;
+
+		rect_min[0] *= factor;
+		rect_max[0] *= factor;
+		rect_min[1] *= factor;
+		rect_max[1] *= factor;
+		
+		rect_min[0] += cx;
+		rect_max[0] += cx;
+		rect_min[1] += cy;
+		rect_max[1] += cy;
+
+	}
 
 	main()
 })
